40daf1324825eede6db52e0f5053fd6148bce658
watabouyosida <58410391+watabouyosida@users.noreply.github.com>
2019-12-13 10:42:44 +0900

    改善1

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message){
     if(message.image) {
       var html =
-        `<div class="message-box" data-id=` + message.id + `>
+        `<div class="message-box" data-id=${message.id} >
           <div class="message-name">
             <div class="message-name__user-name">
               ${message.user_name}
@@ -21,7 +21,7 @@ $(function(){
       return html 
     }else{
       var html =
-        `<div class="message-box"data-id=` + message.id + `>
+        `<div class="message-box" data-id=${message.id} >
            <div class="message-name">
             <div class="message-name__user-name">
               ${message.user_name}
